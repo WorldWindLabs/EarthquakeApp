cc453f05d6662249acb4882428cd08d78894ef42
Benjamin Chang <Ben@Benjamins-MBP.arc.nasa.gov>
2016-06-09 16:08:04 -0700

    Filled in variables for defined functions, messed around with looking at data using pandas(basically better excel)  Please enter the commit message for your changes. Lines starting

--- earthquake-prediction/dev/kodiak.py
+++ earthquake-prediction/dev/kodiak.py
@@ -1,5 +1,3 @@
-# Plot magnetic field data 
-
 import csv
 import numpy as np
 import datetime
@@ -7,10 +5,12 @@ import matplotlib.pyplot as plt
 import urllib
 import matplotlib.dates as mdates
 
-path = '../data/Kodiak-3/'
+path = '/Kodiak-3/'
 # path = '../data/Kodiak-InteleCell/'
 
-def graphXYZ():
+def graphXYZ(vector):
+	
+	f, ax1 = plt.subplots(1, figsize = (10,6))
 	plt.plot(vector.T[0], 'b-')
 	plt.ylabel('X')
 
@@ -23,30 +23,36 @@ def graphXYZ():
 	plt.plot(np.linalg.norm(vector, axis = 1), 'g-')
 	plt.ylabel('norm')
 
-	plt.show()
+        f.savefig(os.path.join(outdir, "-".join(["XYZgraph"]) + ".png"), format='png')
+	plt.close()
+	
 
-def graphcoord(axis):
+def graphcoord(vector, axis):
+	f = plt.figure(figsize = (10, 6))
 	plt.plot(vector.T[axis]/50000, 'b-')
 	plt.ylabel(axis)
+        
+        f.savefig(os.path.join(outdir, "-".join(["graphcoord"]) + ".png"), format='png')
+	plt.close()
 
-	plt.show()
-
-def graphcoord_time(axis):
+def graphcoord_time(vector, axis):
 	time = mdates.drange(datetime.datetime(2014, 10, 21, 16), 
                      datetime.datetime(2014, 10, 25, 16),
                      datetime.timedelta(minutes=15))
 
-	fig = plt.figure()
+	f = plt.figure()
 
 	plt.plot_date(time, vector.T[axis]/50000, 'b-')
 	plt.ylabel(axis)
 	# 2014-10-23T08:30:24Z
 	plt.axvline(datetime.datetime(2014, 10, 23, 8, 30, 24))
 
-	fig.autofmt_xdate()
-	plt.show()
+	f.autofmt_xdate()
+	
+	f.savefig(os.path.join(outdir, "-".join([str.axis, "graphcoord_time"]) + ".png"), format='png')
+	plt.close()
 
-def graphdiff():
+def graphdiff(vector):
 	diff = []
 
 	for i, v in enumerate(vector):
@@ -57,17 +63,19 @@ def graphdiff():
 		if x < 5000:
 			diff[i] = 0
 
+	f = plt.figure(figsize = (10, 6))
 	plt.plot(diff)
-	plt.show()
+	f.savefig(os.path.join(outdir, "-".join(["graphdiff"]) + ".png"), format='png')
+	plt.close()
 
 def loadmagnetic(file):
-	with open(path + file, 'rb') as csvfile:
+	with open(datdir + path + file, 'rb') as csvfile:
 
 		row_count = sum(1 for row in csvfile)
 		vector = np.zeros((row_count, 3))
 		time = []
 
-	with open(path + file, 'rb') as csvfile:
+	with open(datdir + path + file, 'rb') as csvfile:
 		reader = csv.reader(csvfile, delimiter=',', quotechar='|')
 		
 		for i, row in enumerate(reader):
@@ -78,14 +86,14 @@ def loadmagnetic(file):
 
 def getearthquake(minDate, maxDate, origin):
 	
-	minMagnitude = "4.8"
+	minMagnitude = "2.5"
 	maxMagnitude = "10"
 
 	maxdist = "900"
 
 	resourcesUrl = "http://earthquake.usgs.gov/fdsnws/event/1/query?format=csv"
 	dates = "&starttime="+minDate+"&endtime="+maxDate
-	magnitutes = "&minmagnitude=" + minMagnitude + "&maxmagnitude=" + maxMagnitude 
+	magnitutes = "&includeallmagnitudes=true" 
 	local = "&latitude=" + origin['lati'] + "&longitude=" + origin['long'] + "&maxradiuskm=" + maxdist
 
 	opener = urllib.FancyURLopener({})
@@ -105,19 +113,39 @@ def getearthquake(minDate, maxDate, origin):
 
 	return parse_csv(data)
 
+
+xyzgroup1 = [0, 1, 2]
 def plot_interval(init, final, origin):
 	magnetic_data = loadmagnetic(init + "-to-" + final + ".csv")
 
 	earthquakes = getearthquake(init, final, origin)
 
-	# graphXYZ()
+	graphXYZ(magnetic_data['vectors'])
 
-	# graphdiff()
+	graphdiff(magnetic_data['vectors'])
+        
+        for d in xyzgroup1:
+	   graphcoord(magnetic_data['vectors'], d)
 
-	# graphcoord(2)
-
-	# graphcoord_time(t, 0)
-
+	#graphcoord_time(magnetic_data['vectors'], 0)
+	
 kodiak3 = {'lati': "57.747225", 'long': "-152.496467"}
 plot_interval('10-04-16', '17-04-16', kodiak3)
 
+####################################################################################
+'''
+header = ['DateTime', 'X', 'Y', 'Z']
+df = pd.read_csv(os.path.join(datdir,'Kodiak-3', '10-04-16-to-17-04-16.csv'), names = header)
+dft = pd.to_datetime(df.DateTime)
+df.index = dft
+del df['DateTime']
+
+eq = getearthquake('10-04-16', '17-04-16', kodiak3)
+header1 = ['DateTime', 'Latitude', 'Longitude', 'EQ_Magnitude']
+eqdf = pd.DataFrame(eq, columns = header1)
+
+eqt = pd.to_datetime(eqdf.DateTime)
+eqdf.index = eqt
+
+del eqdf['DateTime']
+'''
