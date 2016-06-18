452ae64b409d786db0f331e6e8b211b1f82e13b7
Khaled Sharif <kldsrf@gmail.com>
2016-06-18 21:46:20 +0300

    Added fourier transform script

--- earthquake-prediction/python3-port/loadmagnetic.py
+++ earthquake-prediction/python3-port/loadmagnetic.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 
 def load_magnetic_data(station, begin, end):
-    print("Loading magnetic data",end='')
+    print("Loading magnetic data", end='')
 
     start = process_time()
     path = '../data/' + station + '/' + begin + '-to-' + end + '.csv'
@@ -14,15 +14,15 @@ def load_magnetic_data(station, begin, end):
     if station[:10] == "InteleCell":
         column_names = ['Date', 'X', 'Y', 'Z']
         df = pd.read_csv(path)
-        df.drop('Unnamed: 4',axis=1,inplace=True)
+        df.drop('Unnamed: 4', axis=1, inplace=True)
         df.columns = column_names
 
         # assuming that the Alaska TZ is UTC - 9 hours
         df.index = pd.to_datetime(df.Date, utc=True) + timedelta(hours=9)
 
-        df.interpolate().dropna(how='any',axis=0)
+        df.interpolate().dropna(how='any', axis=0)
 
-        print(" --- took", round(process_time() - start,2), " s")
+        print(" --- took", round(process_time() - start, 2), " s")
 
         return df
 
@@ -34,13 +34,11 @@ def load_magnetic_data(station, begin, end):
         df.index = pd.to_datetime(df['Date'], utc=True) + timedelta(hours=9)
 
         df = df.resample('1T').mean()
-        df = df.interpolate().dropna(how='any',axis=0)
+        df = df.interpolate().dropna(how='any', axis=0)
 
-        print(" --- took", round(process_time() - start,2), " s")
+        print(" --- took", round(process_time() - start, 2), " s")
 
         return df
 
     else:
         print("ERROR - No station recognized")
-
-
