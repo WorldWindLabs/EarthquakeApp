20b379b5f28131142e219c1784021a60b7af0961
Benjamin Chang <ben.chang@hotmail.co.uk>
2016-06-21 14:42:30 -0700

    added anomaly frequencies to major earthquakes (mag > 3) columns

--- earthquake-prediction/python3-port/test_nasa_eps.py
+++ earthquake-prediction/python3-port/test_nasa_eps.py
@@ -1,5 +1,5 @@
 # NASA World Wind Earthquake Data Analysis code
-
+import datetime
 from time import process_time
 import matplotlib.pyplot as plt
 import loadearthquake as eaq
@@ -7,6 +7,7 @@ import loadmagnetic as mag
 import plot as pt
 import pyculiarity.detect_ts as pyc
 import stationsdata as station
+import pandas as pd
 
 # Date format: YYYY-MM-DD
 
@@ -35,5 +36,35 @@ def get_data_frame(column):
 
 
 fX, fY, fZ = get_data_frame('X'),get_data_frame('Y'),get_data_frame('Z')
-pt.plot_earthquake_anomalies_magnetic((fX, fY, fZ), earthquake)
-plt.show()
+
+x_anom, _ = fX
+y_anom, _ = fY
+z_anom, _ = fZ
+
+major_eq = earthquake.ix[earthquake.EQ_Magnitude > 3]
+
+X_anoms = []
+Y_anoms = []
+Z_anoms = []
+for index, row in major_eq.iterrows():
+    end = index
+    start = end - datetime.timedelta(hours= 24)
+    tempX = x_anom[start:end]
+    tempY = y_anom[start:end]
+    tempZ = z_anom[start:end]
+    X_anoms.append(len(tempX.index))
+    Y_anoms.append(len(tempY.index))
+    Z_anoms.append(len(tempZ.index))
+    # print(len(temp.index))
+x_anomalies = pd.Series(X_anoms)
+y_anomalies = pd.Series(Y_anoms)
+z_anomalies = pd.Series(Z_anoms)
+major_eq['X_anomalies'] = x_anomalies.values
+major_eq['Y_anomalies'] = y_anomalies.values
+major_eq['Z_anomalies'] = z_anomalies.values
+
+print(major_eq.head())
+
+# pt.plot_earthquake_anomalies_magnetic((fX, fY, fZ), earthquake)
+# plt.show()
+
